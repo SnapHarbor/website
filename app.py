299015033c9c1bce4c016d2d246d87f327f327e0
SnapHarbor <122692264+SnapHarbor@users.noreply.github.com>
2025-08-12 23:12:05 +0300

    asd

--- app.py
+++ app.py
@@ -7,6 +7,20 @@ app = Flask(__name__)
 def main_page():
     return render_template('main.html')
 
+<<<<<<< Updated upstream
+=======
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+@app.route('/rating')
+def rating_page():
+    return render_template('rating.html')
+
+@app.route('/players')
+def players_page():
+    return render_template('players.html')
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     app.run()
